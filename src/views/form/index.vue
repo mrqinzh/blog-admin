bd271b01dfa3dddd439b87c9640b75cf0f1e94c0
qinzhihong <1552589784@qq.com>
2021-08-10 14:30:55 +0800

    20210810

---
+++ src/views/form/index.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="app-container">
+    <el-form ref="form" :model="form" label-width="120px">
+      <el-form-item label="姓名">
+        <el-input v-model="form.name" />
+      </el-form-item>
+      <el-form-item label="组织">
+        <el-select v-model="form.region" placeholder="请选择你的zone">
+          <el-option label="上海" value="shanghai" />
+          <el-option label="北京" value="beijing" />
+        </el-select>
+      </el-form-item>
+      <el-form-item label="日期">
+        <el-col :span="11">
+          <el-date-picker v-model="form.date1" type="date" placeholder="选择日期" style="width: 100%;" />
+        </el-col>
+        <el-col :span="2" class="line">-</el-col>
+        <el-col :span="11">
+          <el-time-picker v-model="form.date2" type="fixed-time" placeholder="选择时间" style="width: 100%;" />
+        </el-col>
+      </el-form-item>
+      <el-form-item label="活动区域">
+        <el-input v-model="form.desc" type="textarea" />
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="onSubmit">创建</el-button>
+        <el-button @click="onCancel">取消</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      form: {
+        name: '',
+        region: '',
+        date1: '',
+        date2: '',
+        delivery: false,
+        type: [],
+        resource: '',
+        desc: ''
+      }
+    }
+  },
+  methods: {
+    onSubmit() {
+      this.$message('submit!')
+    },
+    onCancel() {
+      this.$message({
+        message: 'cancel!',
+        type: 'warning'
+      })
+    }
+  }
+}
+</script>
+
+<style scoped>
+.line{
+  text-align: center;
+}
+</style>
+
